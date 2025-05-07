16c4ef1bc6177c78ae0722c306c0d6fbb54df9b8
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2025-05-07 14:59:24 +0200

    User model is created and tested, Email constraint is working.

---
+++ ZRoomLoginLibrary/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZRoomLoginLibrary.Models;
+
+namespace ZRoomLoginLibrary.Repositories
+{
+    public class UserRepository
+    {
+        private readonly string _connectionString = "Server=localhost;Database=ZRoomLoginService;Integrated Security=True;Encrypt=False";
+
+        public List<User> GetAll()
+        {
+            List<User> outputList = new();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sqlQuery = "SELECT Email, Name, StudentId, ClassId FROM Users";
+
+                SqlCommand command = new(sqlQuery, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    outputList.Add(new User(reader.GetString(0), reader.GetString(1),
+                                            reader.GetInt32(2), reader.GetInt32(3)));
+                }
+
+                return outputList;
+            }
+        }
+
+    }
+}
---
+++ ZRoomUnitTests/LoginSystemUserUnitTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZRoomLoginLibrary.Models;
+
+namespace ZRoomUnitTests
+{
+    [TestClass]
+    public class LoginSystemUserUnitTests
+    {
+        [TestMethod]
+        public void Constructor_SetsPropertiesCorrect_ShouldPass()
+        {
+            // Arrange
+            string email = "testmail@edu.zealand.dk";
+            string name = "anders";
+            int studyId = 1;
+            int classId = 1;
+
+            // Act 
+            User testUser = new(email, name, studyId, classId);
+
+            // Assert
+            Assert.AreEqual(email, testUser.Email);
+            Assert.AreEqual(name, testUser.Name);
+            Assert.AreEqual(studyId, testUser.StudentId);
+            Assert.AreEqual(classId, testUser.ClassId);
+        }
+
+        [TestMethod]
+        public void ChangeValueOfProper_CorrectNewValue_ShouldPass()
+        {
+            // Arrange
+            string newName = "Svend";
+            User testUser = new("test@edu.zealand.dk", "Arne", 1, 1);
+            string expectedResult = newName;
+
+            // Act
+            testUser.Name = newName;
+            var actualResult = testUser.Name;
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void InvalidEmail_ShouldThrowException()
+        {
+            // Arrange
+            string wrongEmail = "test@hotmail.com";
+
+
+            // Act
+            Action expectedException = () => new User(wrongEmail, "gg", 1, 2);
+            Debug.WriteLine(expectedException);
+
+            // Assert
+            Assert.ThrowsException<ArgumentException>(expectedException);
+        }
+    }
+}

5625421efebfe4610a415033c58f347995eb9744
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2025-05-21 12:09:32 +0200

    Swagger kan nu bruges i deployment

--- ZRoomBackendApi/Program.cs
+++ ZRoomBackendApi/Program.cs
@@ -60,11 +60,10 @@ namespace ZRoomBackendApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
 
             app.UseAuthorization();
 
